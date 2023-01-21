8126490ddbf2f560e402424b8c82b130613fc657
TonyStr <tony.stroemsnaes@gmail.com>
2023-01-21 15:39:09 +0100

    feat(userform): split selects up into steps

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 
-function Select({ values, name }) {
+function Select({ values, name, selected, onChange }) {
+	// <label> element has to stay for accessibility purposes, however
+	// it can be hidden with the className "sr-only"
 	return (
 		<div className="flex flex-col m-3">
 			<label htmlFor={name} className="font-bold pb-1">{name}</label>
-			<select name={name} className="py-1 px-3 cursor-pointer rounded hover:outline-marine-dark">
+			<select 
+				name={name}
+				onChange={(e) => onChange(e.target.value)}
+				value={selected ?? ''}
+				className="py-1 px-3 cursor-pointer rounded hover:outline-marine-dark"
+			>
 				{values.map((value) => (
 					<option value={value} key={value}>
 						{value}
@@ -15,17 +23,62 @@ function Select({ values, name }) {
 }
 
 export default function UserForm() {
-	const dialects = 'Østlandsk, Vestlandsk, Trøndersk, Nordnorsk, Annet'.split(', ');
-	const ageGroups = '18-29, 30-39, 40-49, 50-59, 60+'.split(', ');
-	const languages = 'Norsk, Arabisk, Albansk, Pakistansk, Annet'.split(', ');
-	const genders = 'Mann, Kvinne, Annet'.split(', ');
+	// The values that can be selected
+	const dialects = 'Velg dialekt, Østlandsk, Vestlandsk, Trøndersk, Nordnorsk, Annet'.split(', ');
+	const ageGroups = 'Velg aldersgruppe, 18-29, 30-39, 40-49, 50-59, 60+'.split(', ');
+	const languages = 'Velg morsmål, Norsk, Arabisk, Albansk, Pakistansk, Annet'.split(', ');
+	const genders = 'Velg kjønn, Mann, Kvinne, Annet'.split(', ');
+
+	// Selected values
+	const [gender, setGender] = useState(null);
+	const [ageGroup, setAgeGroup] = useState(null);
+	const [language, setLanguage] = useState(null);
+	const [dialect, setDialect] = useState(null);
+
+	const [step, setStep] = useState(0);
+	const steps = [
+		<Select 
+			name="Kjønn" 
+			values={genders} 
+			selected={gender}
+			onChange={(value) => setGender(value)}
+		/>,
+		<Select 
+			name="Aldersgruppe" 
+			values={ageGroups} 
+			selected={ageGroup}
+			onChange={(value) => setAgeGroup(value)}
+		/>,
+		(<div>
+			<Select 
+				name="Morsmål" 
+				values={languages} 
+				selected={language}
+				onChange={(value) => setLanguage(value)}papegøye
+			/>
+			{language === 'Norsk' && (
+				<Select 
+					name="Dialekt" 
+					values={dialects} 
+					selected={dialect}
+					onChange={(value) => setDialect(value)}
+				/>
+			)}
+		</div>),
+	];
 
 	return (
-		<div className="flex place-items-center justify-center flex-grow">
-			<Select values={dialects} name="Dialekt" />
-			<Select values={ageGroups} name="Aldersgruppe" />
-			<Select values={languages} name="Morsmål" />
-			<Select values={genders} name="Kjønn" />
+		<div className="flex flex-col place-items-center justify-center flex-grow">
+			{steps[step]}
+			<button onClick={() => setStep(prev => prev + 1)} className="bg-blue-400 px-4 font-bold py-2 text-xl text-white mt-8">
+				Gå videre
+			</button>
+			<button onClick={() => setStep(prev => prev - 1)} className="bg-gray-200 px-5 py-1 mt-2">
+				Hopp over
+			</button>
+			<div className="max-w-md text-gray-300 italic mt-14">
+				Vi samler ikke inn noen personlige data fra deg, denne infoen blir kun brukt til å systematisere data.
+			</div>
 		</div>
 	);
 }
