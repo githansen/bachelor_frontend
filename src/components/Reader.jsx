c6b42ed3c7e69048b6110498f6c1fb82371e259e
TonyStr <tony.stroemsnaes@gmail.com>
2023-01-23 13:30:11 +0100

    feat(reader): stateful components in reader

--- src/components/Reader.jsx
+++ src/components/Reader.jsx
@@ -1,45 +1,64 @@
+import { useState } from 'react';
 
-function Info() {
+function Info({ state }) {
+	const heading = {
+		idle: 'Klar til opptak?',
+		recording: 'Les opp teksten under',
+		completed: 'Bra jobbet!',
+	}[state];
+
+	const infoText = {
+		idle: 'Her skal du lese opp teksten i bosken under og spille inn lyden. Uttal ordene så tydelig som mulig men husk å være deg selv.',
+		recording: '',
+		completed: 'Da er din innspilling ferdig. Nå kan du velge om du vil høre ditt opptak, prøve på nytt eller sende inn til oss!',
+	}[state];
+
 	return (
 		<div>
-			<h1 className="text-2xl font-semibold text-marine-dark">Klar til opptak?</h1>
-			<p className="max-w-md text-sm">
-				Her skal du lese opp teksten i bosken under og spille inn lyden. 
-				Uttal ordene så tydelig som mulig men husk å være deg selv. 
-			</p>
+			<h1 className="text-2xl font-semibold text-marine-dark">
+				{heading}
+			</h1>
+			{infoText && (<p className="max-w-md text-sm">
+				{infoText}
+			</p>)}
 		</div>
 	);
 }
 
-function Controls() {
+function Controls({ state, setState }) {
 	return (
 		<div>
-			<button className="bg-gray-200 border-2 border-gray-300 font-semibold px-4 py-2 mr-2">
+			<button className="bg-gray-200 border-2 border-gray-300 font-semibold px-4 py-2 mr-2 hover:border-marine-dark">
 				Ny tekst
 			</button>
-			<button className="bg-gray-200 border-2 border-gray-300 font-semibold px-4 py-2 mr-2">
+			<button
+				onClick={() => setState('recording')}
+				className="bg-gray-200 border-2 border-gray-300 font-semibold px-4 py-2 mr-2 hover:border-marine-dark"
+			>
 				Start opptak
 			</button>
 		</div>
 	);
 }
 
-function TextPanel() {
+function TextPanel({ state }) {
 	return (
-		<div className="border-gray-300 bg-gray-200 text-xl font-semibold p-16 text-center border-2">
+		<div className={`text-2xl font-semibold p-16 text-center border-2 ${state === 'recording' ? 'bg-yellow-300 border-yellow-500' : 'bg-gray-200 border-gray-300'}`}>
 			Uten hensyn til en prosentvis sikkerhetsforskrift påvirkes betingelsene hva gjelder oppgjøret. Under forutsetning av en offensiv konflikthåndtering realiseres trusselbildet med hensyn til styrkedisponeringen.
 		</div>
 	);
 }
 
 export default function Reader() {
+	const [state, setState] = useState('idle'); // idle | recording | completed
+
 	return (
 		<div className="w-[800px] mx-auto mt-14">
 			<div className="flex justify-between mb-4">
-				<Info />
-				<Controls />
+				<Info state={state} />
+				<Controls state={state} setState={setState} />
 			</div>
-			<TextPanel />
+			<TextPanel state={state} />
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ module.exports = {
 			'blue-400': '#0088fa',
 			'gray-200': '#f2f2f2',
 			'gray-300': '#C2C2C2',
+			'yellow-300': '#ffe87b',
+			'yellow-500': '#FFD923',
 		},
 		extend: {},
 	},
