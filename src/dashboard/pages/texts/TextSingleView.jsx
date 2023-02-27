8f9673af28635c64fb8bd7f6821a144f7357eb05
Sefa <info@diestus.com>
2023-02-27 17:29:35 +0100

    update

--- src/dashboard/pages/texts/TextSingleView.jsx
+++ src/dashboard/pages/texts/TextSingleView.jsx
@@ -1,3 +1,5 @@
+//React library
+import { NavLink } from 'react-router-dom';
 //Animation library
 import { motion as m } from 'framer-motion';
 //Shared
@@ -5,7 +7,8 @@ import DashMenu from '../../components/shared/DashMenu';
 import { ContributionTable } from '../../components/table/ContributionTable';
 import '@/styles/tableStyle.css';
 //Icons
-import { PlayCircleIcon } from '@heroicons/react/24/outline';
+import { DocumentIcon } from '@heroicons/react/24/solid';
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
 
 export default function TextSingleView() {
     const columns = [
@@ -16,7 +19,6 @@ export default function TextSingleView() {
             format: (value) => '',
             type: 'audio',
         },
-        { accessor: 'text', label: 'Tekst' },
         { accessor: 'target', label: 'Målgruppe' },
         { accessor: 'contribution_date', label: 'Innlest dato' },
         {
@@ -58,8 +60,38 @@ export default function TextSingleView() {
             <DashMenu />
 
             <div className="p-12 w-full">
-                <div className="mb-5 items-end">
-                    <div className="inline-flex gap-3">
+                <div className="flex justify-between mb-5 items-center">
+                    <div className="mb-5 items-end">
+                        <m.nav
+                            initial={{ y: '-10px', opacity: 0 }}
+                            animate={{ y: '0', opacity: 1 }}
+                            transition={{
+                                duration: 0.3,
+                                ease: 'easeOut',
+                            }}
+                            className="flex mb-6 text-xlp"
+                            aria-label="Breadcrumb"
+                        >
+                            <ol className="inline-flex items-center space-x-1 md:space-x-3">
+                                <li className="inline-flex items-center text-bolge hover:opacity-60 transition duration-200">
+                                    <NavLink
+                                        to="/gdsadmin/text"
+                                        className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+                                    >
+                                        <DocumentIcon className="h-4 w-4 mr-1" />
+                                        Tekster
+                                    </NavLink>
+                                </li>
+                                <li aria-current="page" className="text-bolge">
+                                    <div className="flex items-center">
+                                        <ChevronRightIcon className="h-5 w-5 mr-1" />
+                                        <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+                                            Texttittel
+                                        </span>
+                                    </div>
+                                </li>
+                            </ol>
+                        </m.nav>
                         <m.h2
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
@@ -69,21 +101,22 @@ export default function TextSingleView() {
                             }}
                             className="text-xlh2 font-feteste leading-10 mb-3"
                         >
-                            Tekst: Random tittel
+                            <span className="font-normal">Tekst:</span>{' '}
+                            Textittel
                         </m.h2>
+                        <m.p
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{
+                                duration: 0.3,
+                                ease: 'easeOut',
+                            }}
+                            className="text-xlp mt-2"
+                        >
+                            **Her kommer den riktige teksten** Dette er en
+                            random tekst generert for å være en verdi her.
+                        </m.p>
                     </div>
-                    <m.p
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{
-                            duration: 0.3,
-                            ease: 'easeOut',
-                        }}
-                        className="text-xlp mt-2"
-                    >
-                        **Her kommer den riktige teksten** Dette er en random
-                        tekst generert for å være en verdi her.
-                    </m.p>
                 </div>
                 <div>
                     <ContributionTable
