824f4d09e3da82ce674da15db04222e48a925c58
Johan Hustoft Hansen <s349880@oslomet.no>
2023-03-03 14:20:54 +0100

    test: -secure false

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
 			'/api': {
 				target: apiTarget,
 				changeOrigin: true,
-				secure: usingLocalApi ? false : true,
+				secure: usingLocalApi ? false : false,
 				ws: true,
 				rewrite: (path) => path.replace(/^\/api/, ''),
 			} 
